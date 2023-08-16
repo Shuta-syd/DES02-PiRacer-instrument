abe2a8c110ab4d16e4adba9cca1170b82af4d4ba
Shuta-syd <shuta.ogura0921@gmail.com>
2023-08-16 17:26:18 +0200

    [update] dbus python code update

--- app/d-bus/d-bus-transmitter.py
+++ app/d-bus/d-bus-transmitter.py
@@ -25,17 +25,19 @@ class DbusService(object):
   """
   def __init__(self):
     self._can = can.interface.Bus(channel=can_interface, bustype='socketcan')
+    self._rpm = 0
 
   def getRpm(self) -> int:
     message = self._can.recv()
     if message is not None and message.arbitration_id == rpm_canId:
       rpm = int.from_bytes(message.data[:2], byteorder='little', signed=False)
+      self._rpm = rpm
       return rpm
     return 0
 
-  def getSpeed(self, rpm) -> int:
-    print("rpm: ", rpm);
-    speed = rpm * wheel_circumference
+  def getSpeed(self) -> int:
+    print("rpm: ", self.rpm);
+    speed = self._rpm * wheel_circumference
     return speed
 
 
