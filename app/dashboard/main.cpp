026bfab28d6193eb48781a25a7a9eb9f603cfdd8
KKWANH <kwanho0096@gmail.com>
2023-08-17 15:35:54 +0200

    :sparkles: #20 [Design Update] main receiving part

--- app/dashboard/main.cpp
+++ app/dashboard/main.cpp
@@ -16,6 +16,11 @@
 //  library for QT animation
 # include   <QPropertyAnimation>
 
+// Library for JSON parsing
+# include   <QJsonDocument>
+# include   <QJsonObject>
+# include   <QJsonValue>
+
 
 //  macros
 # define    FAILURE     -1
@@ -63,67 +68,92 @@ int         main(
     if (engine.rootObjects().isEmpty())
         return  (FAILURE);
 
-    // //  Find the root object
-    // QObject* root = engine.rootObjects().first();
+    //  Find the root object
+    QObject* root = engine.rootObjects().first();
 
-    // //  Find valueSource in the root object
-    // QObject* valueSource = root->findChild<QObject*>("valueSource");
-    // if (!valueSource)
-    // {
-    //     qWarning() << "Cannot find object named 'valueSource'";
-    //     return  (FAILURE);
-    // }
+    //  Find valueSource in the root object
+    QObject* valueSource = root->findChild<QObject*>("valueSource");
+    if (!valueSource)
+    {
+        qWarning() << "Cannot find object named 'valueSource'";
+        return  (FAILURE);
+    }
 
-    // //  Create and initialize the socket
-    // QTcpSocket* socket = new QTcpSocket();
-    // QObject::connect(socket, &QTcpSocket::connected, []()
-    // {
-    //     qDebug() << "Connected to the server";
-    // });
+    //  Create and initialize the socket
+    QTcpSocket* socket = new QTcpSocket();
+    QObject::connect(socket, &QTcpSocket::connected, []()
+    {
+        qDebug() << "Connected to the server";
+    });
 
-    // //  Try to connect to the server
-    // socket->connectToHost(HOST, PORT);
-    // if (!socket->waitForConnected(3000))
-    // {
-    //     qDebug() << "Error: " << socket->errorString();
-    //     return  (FAILURE);
-    // }
+    //  Try to connect to the server
+    socket->connectToHost(HOST, PORT);
+    if (!socket->waitForConnected(3000))
+    {
+        qDebug() << "Error: " << socket->errorString();
+        return  (FAILURE);
+    }
 
-    // //  Create animations for smooth transitions
-    // QPropertyAnimation* speedAnimation = new QPropertyAnimation(valueSource, "spd");
-    // speedAnimation->setDuration(400); // You can adjust the duration for smoothness
-    // QPropertyAnimation* rpmAnimation = new QPropertyAnimation(valueSource, "rpm");
-    // rpmAnimation->setDuration(400); // You can adjust the duration for smoothness
+    //  Create animations for smooth transitions
+    QList<QList<QString> properties = [
+        ["throttle",            "float",    "throttle"],
+        ["steering",            "float",    "steering"],
+        ["indicator",           "float",    "indicator"],
+        ["battery_voltage",     "float",    "voltage"],
+        ["power_consumption",   "float",    "consumption"],
+        ["battery_current",     "float",    "current"],
+        ["battery_current",     "float",    "current"],
+        ["battery_level",       "float",    "level"],
+        ["battery_hour",        "float",    "hour"],
+        ["speed",               "short",    "speed"],
+        ["rpm",                 "short",    "rpm"],
+        ["ip_address",          "string",    "ip_address"],
+        ["curtime",             "string",   "curtime"],
+    ]
+    QList<QPropertyAnimation> animations;
+    for (int i=0; i<properties.size(); i++) {
+        animations[i] = new QPropertyAnimation(valueSource, properties[i][0])
+        animations[i]->setDuration(400);
+    }
 
-    // //  Update the properties when data is ready
-    // QStringList logList;
-    // QObject::connect(socket, &QTcpSocket::readyRead, [socket, valueSource, speedAnimation, rpmAnimation]() {
-    //     QTextStream _T(socket);
-    //     QString _msg = _T.readAll();
-    //     QStringList _list = _msg.split(",");
-    //     if (_list.size() == 2) {
-    //         // Smooth transition for speed
-    //         speedAnimation->setEndValue(_list[0].toDouble());
-    //         if (speedAnimation->state() != QPropertyAnimation::Running)
-    //             speedAnimation->start();
-    //         // Smooth transition for rpm
-    //         rpmAnimation->setEndValue(_list[1].toDouble());
-    //         if (rpmAnimation->state() != QPropertyAnimation::Running)
-    //             rpmAnimation->start();
-    //     } else {
-    //         qWarning() << "Unexpected message format: " << _msg;
-    //     }
-    // });
+    QStringList logList;
+    QObject::connect(socket, &QTcpSocket::readyRead, [socket, valueSource, speedAnimation, rpmAnimation]() {
+        QTextStream     _T(socket);
+        QString         _msg = _T.readAll();
+
+        QJsonDocument   _json = QJsonDocument::fromJson(_msg.toUtf8());
+        if (!_json.isNull())
+        {
+            QJsonObject _jsonObj = _json.object();
+            for (int i=0; i<properties.size(); i++)
+            {
+                auto _data = _jsonObj[properties[i]];
+                auto _updatedData;
+                if (properties[i][1] == "float")
+                    _updatedData = _data.toDouble();
+                else if (properties[i][1] == "string")
+                    _updatedData = _data;
+                else if (properties[i][1] == "short")
+                    _updatedData = _data.toInt();
+
+                animations[i]->setEndValue(_updatedData);
+                if (animations[i]->state() != QPropertyAnimation::Running)
+                    animations[i]->start();
+            }
+        }
+        else
+            qWarning() << "Invalid JSON: " << _msg;
+    });
 
     int result = app.exec();
 
-    // QFile file("log/log.txt");
-    // if (file.open(QIODevice::WriteOnly))
-    // {
-    //     QTextStream stream(&file);
-    //     for (const QString &log : logList)
-    //         stream << log << "\n";
-    // }
+    QFile file("log/log.txt");
+    if (file.open(QIODevice::WriteOnly))
+    {
+        QTextStream stream(&file);
+        for (const QString &log : logList)
+            stream << log << "\n";
+    }
 
     return result;
 }
