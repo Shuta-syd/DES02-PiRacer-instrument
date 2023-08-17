87e0f0b2fc460a65dd54260475b542d9620a876b
Shuta-syd <shuta.ogura0921@gmail.com>
2023-08-17 17:02:09 +0200

    [fix] sending data time term is changed

--- app/dashboard/dbusclient.cpp
+++ app/dashboard/dbusclient.cpp
@@ -2,7 +2,7 @@
 #include <QDBusReply>
 #include <QDebug>
 
-#define CLOCK_TIME 0.05
+#define CLOCK_TIME 1
 
 DBusClient::DBusClient(QObject *parent)
     : QObject{parent}, _dbus(QDBusConnection::sessionBus()), _iface(Q_NULLPTR), _speed(0), _rpm(0)
