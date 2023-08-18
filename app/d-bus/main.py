7b43d14a3b543945d484c1033db8df11b615fdb0
Shuta-syd <shuta.ogura0921@gmail.com>
2023-08-18 17:57:04 +0200

    [update] main.py updated

--- app/d-bus/main.py
+++ app/d-bus/main.py
@@ -3,6 +3,7 @@ from multiprocessing  import Process
 from piracer.vehicles import PiRacerStandard
 from  battery_service import battery_service_process
 from dbus_service import dbus_service_process
+from car_control import car_control
 from gamepads import ShanWanGamepad
 
 if __name__ == '__main__':
