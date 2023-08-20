849c146707b33353ca93d35dd9f77898e7bd4a6e
Shuta-syd <shuta.ogura0921@gmail.com>
2023-08-20 19:14:19 +0200

    [debug] battery_service.py debug

--- app/d-bus/battery_service.py
+++ app/d-bus/battery_service.py
@@ -7,35 +7,35 @@ class BatteryService(object):
       <node>
           <interface name='com.dbus.batteryService'>
               <method name='getConsumption'>
-                  <arg type='f' name='response' direction='out'/>
+                  <arg type='s' name='response' direction='out'/>
               </method>
               <method name='getVoltage'>
-                  <arg type='i' name='response' direction='out'/>
+                  <arg type='s' name='response' direction='out'/>
               </method>
               <method name='getCurrent'>
-                <arg type='f' name='response' direction='out'/>
+                <arg type='s' name='response' direction='out'/>
               </method>
           </interface>
       </node>
   """
   def __init__(self, vehicle):
-    self._current = 0
-    self._voltage = 0
-    self._consumption = 0
-    self._level = 0
+    self._current = ''
+    self._voltage = ''
+    self._consumption = ''
+    self._level = ''
     self._vehicle = vehicle
+    print(vehicle)
 
-  def getVoltage(self) -> int:
-    # _voltage          = round(self._vehicle.get_battery_voltage(),1) # in V
-    _voltage = 42
+  def getVoltage(self) -> str:
+    _voltage          = str(round(self._vehicle.get_battery_voltage(),1)) # in V
     return _voltage
 
-  def getConsumption(self) -> float:
-    _consumption      = round(self._vehicle.get_power_consumption(),1) # in W
+  def getConsumption(self) -> str:
+    _consumption      = str(round(self._vehicle.get_power_consumption(),1)) # in W
     return _consumption
 
-  def getCurrent(self) -> float:
-    _current = round(self._vehicle.get_battery_current(),1) # in mA
+  def getCurrent(self) -> str:
+    _current = str(round(self._vehicle.get_battery_current(),1)) # in mA
     return _current
 
 def battery_service_process(vehicle):
