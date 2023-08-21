404671c27be24e858bbbadcc850e366e9c9a1209
Shuta-syd <shuta.ogura0921@gmail.com>
2023-08-21 14:38:45 +0200

    [fix] monitor.py error fix

--- app/d-bus/monitor.py
+++ app/d-bus/monitor.py
@@ -6,7 +6,7 @@ def monitor_thread(processes, piracer):
         for p in processes:
             if not p.is_alive():
                 print(f"Process {p.name} has terminated unexpectedly!")
-                if p.name is 'python3_car_control':
+                if p.name == 'python3_car_control':
                     piracer.set_steering_percent(0)
                     piracer.set_throttle_percent(0)
         time.sleep(1)
