7d755406f8a12159d478a364ac828f70cc19671a
Shuta-syd <shuta.ogura0921@gmail.com>
2023-08-21 15:55:51 +0200

    [update] monitor.py update

--- app/d-bus/monitor.py
+++ app/d-bus/monitor.py
@@ -4,6 +4,7 @@ from battery_service import battery_service_process
 from car_control import car_control
 from dbus_service import dbus_service_process
 from multiprocessing import Process
+from setproctitle import setproctitle
 
 def restart_process(target, args, name):
     new_process = Process(target=target, args=args, name=name)
@@ -19,10 +20,13 @@ def monitor_thread(processes, piracer, shanwan_gamepad):
                     piracer.set_steering_percent(0)
                     piracer.set_throttle_percent(0)
                     new_process = restart_process(target=car_control, args=(piracer, shanwan_gamepad), name=p.name)
+                    setproctitle("python3_car_control")
                 elif p.name == 'python3_battery_process':
                     new_process = restart_process(target=battery_service_process, args=(piracer,), name=p.name)
+                    setproctitle("python3_battery_process")
                 elif p.name == 'python3_dbus_process':
                     new_process = restart_process(target=dbus_service_process, args=(), name=p.name)
+                    setproctitle("python3_dbus_process")
                 processes.remove(p)
                 processes.append(new_process)
         time.sleep(1)
