7995856e4b1a9c019b491389eeecf226a75cb86c
Shuta-syd <shuta.ogura0921@gmail.com>
2023-08-21 14:49:17 +0200

    [add] monitor.py restart function add

--- app/d-bus/monitor.py
+++ app/d-bus/monitor.py
@@ -1,6 +1,11 @@
 import time
 from multiprocessing import Process
 
+def restart_process(process):
+    new_process = Process(target=process._target, args=process._args, name=process.name)
+    new_process.start()
+    return new_process
+
 def monitor_thread(processes, piracer):
      while True:
         for p in processes:
@@ -9,4 +14,7 @@ def monitor_thread(processes, piracer):
                 if p.name == 'python3_car_control':
                     piracer.set_steering_percent(0)
                     piracer.set_throttle_percent(0)
+                new_process = restart_process(p)
+                processes.remove(p)
+                processes.append(new_process)
         time.sleep(1)
