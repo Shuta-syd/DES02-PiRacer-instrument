ab11351dd7410967ca3a68cc3025f43905ed8f64
Shuta-syd <shuta.ogura0921@gmail.com>
2023-08-21 12:09:33 +0200

    [update] python process name added

--- app/d-bus/main.py
+++ app/d-bus/main.py
@@ -11,17 +11,17 @@ if __name__ == '__main__':
   piracer         = PiRacerStandard()
   shanwan_gamepad = ShanWanGamepad()
 
-  car_control_process = Process(target=car_control, args=(piracer, shanwan_gamepad))
+  car_control_process = Process(target=car_control, args=(piracer, shanwan_gamepad), name='python3_car_control')
   car_control_process.start()
 
-  battery_process = Process(target=battery_service_process, args=(piracer,))
+  battery_process = Process(target=battery_service_process, args=(piracer,), name='python3_batter_process')
   battery_process.start()
 
-  dbus_process = Process(target=dbus_service_process)
+  dbus_process = Process(target=dbus_service_process, name='python3_dbus_process')
   dbus_process.start()
 
   processes = [car_control, battery_process, dbus_process]
-  monitoring_process = Process(target=monitor_processes, args=(processes,))
+  monitoring_process = Process(target=monitor_processes, args=(processes,), name='python3_monitor_process')
   monitoring_process.start()
 
   car_control_process.join()
