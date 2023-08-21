99e58dfe2f0954fc8518674201cfdb2245813691
Shuta-syd <shuta.ogura0921@gmail.com>
2023-08-21 12:36:32 +0200

    [debug] monitor.py debug print deleted

--- app/d-bus/monitor.py
+++ app/d-bus/monitor.py
@@ -4,7 +4,6 @@ from multiprocessing import Process
 def monitor_thread(processes):
      while True:
         for p in processes:
-            print(p)
             if not p.is_alive():
                 print(f"Process {p.name} has terminated unexpectedly!")
         time.sleep(1)
