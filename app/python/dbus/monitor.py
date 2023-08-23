c9aa941b27d8dd39f15dad7fbb51d026edc61701
Shuta-syd <shuta.ogura0921@gmail.com>
2023-08-23 17:40:30 +0200

    [debug] monitor.py debug

--- app/python/dbus/monitor.py
+++ app/python/dbus/monitor.py
@@ -18,10 +18,10 @@ def monitor_thread(processes, piracer):
                 if p.name == 'python3_car_control':
                     piracer.set_steering_percent(0)
                     piracer.set_throttle_percent(0)
-                    new_process = restart_process(target=car_control, args=(), name=p.name)
+                    new_process = restart_process(target=car_control, args=(piracer, ), name=p.name)
                     setproctitle("python3_car_control")
                 elif p.name == 'python3_battery_process':
-                    new_process = restart_process(target=battery_service_process, args=(),  name=p.name)
+                    new_process = restart_process(target=battery_service_process, args=(piracer, ),  name=p.name)
                     setproctitle("python3_battery_process")
                 elif p.name == 'python3_dbus_process':
                     new_process = restart_process(target=dbus_service_process, args=(), name=p.name)
