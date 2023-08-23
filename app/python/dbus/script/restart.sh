1599ae33860f86b91b645a48fe3738d96c011dd3
Shuta-syd <shuta.ogura0921@gmail.com>
2023-08-23 16:54:04 +0200

    [mod] monitor script changed

--- app/python/dbus/script/restart.sh
+++ app/python/dbus/script/restart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo pkill -f "python3"
-source /home/seame01/workspace/DES02-PiRacer-instrument/app/venv/bin/activate
-python3 /home/seame01/workspace/DES02-PiRacer-instrument/app/d-bus/main.py
+source /home/seame01/workspace/DES02-PiRacer-instrument/app/python/venv/bin/activate
+python3 /home/seame01/workspace/DES02-PiRacer-instrument/app/python/main.py
