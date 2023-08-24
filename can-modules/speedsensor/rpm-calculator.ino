c4ee3fdf96eb369cf6082913f99adcf8c2d30f5e
Shuta-syd <shuta.ogura0921@gmail.com>
2023-08-24 12:44:28 +0200

    [update] rpm-calculator.ino updated

--- can-modules/speedsensor/rpm-calculator.ino
+++ can-modules/speedsensor/rpm-calculator.ino
@@ -7,22 +7,29 @@ MCP_CAN CAN(10);
 /** values to calculate RPM */
 #define PPR 20
 #define PI 3.1415926535897932384626433832795
-#define ZERO_TIMEOUT    130000  // Timeout period(microsecond) for RPM reset
 #define WheelDiameter 65.0 // [mm]
 #define SpeedSensorDiameter 20.0 // [mm]
+#define RPM_SENSOR_MAX 1800 // Max. RPM of sensor
 
+double sample_rate = 0.0;
 double elapsedTimeAvg = 0.0;
 double elapsedTimeSum = 100000.0;
 double elapsedTime = 0;
 double prevTime = 0;
-unsigned int zeroDebounce = 0;
+
 unsigned long frqRaw = 0;
 unsigned short RPM_s = 0;
 unsigned short RPM_w = 0;
 unsigned short speed = 0;
+int tmpReadCnt = 0;
 int readingCnt = 1;
 int purseCnt = 1;
 
+uint8_t data[8];
+int can_id = 0x125;
+int can_dlc = 8;
+double C = WheelDiameter * PI / 1000;
+int status = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -31,46 +38,38 @@ void setup() {
   CAN.begin(MCP_ANY, CAN_125KBPS, MCP_8MHZ);
   CAN.setMode(MCP_NORMAL);
 
+  // Nyquist-Shannon Sampling Theorem
+  sample_rate = (1 / ( 2 * (RPM_SENSOR_MAX / 60))) * 1000; // calculate sample rate (Nyquist) in ms
+
   attachInterrupt(digitalPinToInterrupt(SPEED_SENSOR_PIN), purseCounter, RISING);
 }
 
 void loop() {
-  uint8_t data[8];
-  int can_id = 0x125;
-  int can_dlc = 8;
-  double C = WheelDiameter * PI / 1000;
 
-  double prev_cycle_time = micros();
-  double current_time = micros();
-  frqRaw = 1000000 * 1000 / elapsedTimeAvg;
+  if (millis() - millis_before >= sample_rate) {
+    detachInterrupt(digitalPinToInterrupt(SPEED_SENSOR_PIN));
+    frqRaw = 1000000 * 1000 / elapsedTimeAvg;
 
-  if (
-    elapsedTime > (ZERO_TIMEOUT - zeroDebounce) ||
-    (current_time - prev_cycle_time) > (ZERO_TIMEOUT - zeroDebounce)||
-    elapsedTimeAvg < 1
-    ) {
-    frqRaw = 0;
-     zeroDebounce = 2000;
-  } else  zeroDebounce = 0;
+    RPM_s = (frqRaw * 60 / PPR) / 1000;
+    RPM_w = RPM_s * (SpeedSensorDiameter / WheelDiameter);
+    speed = RPM_w * C;
 
-  RPM_s = (frqRaw * 60 / PPR) / 1000;
-  RPM_w = RPM_s * (SpeedSensorDiameter / WheelDiameter);
-  speed = RPM_w * C;
+    Serial.print("RPM: "); Serial.print(RPM_w); Serial.print(" Speed [m/min]: "); Serial.println(speed);
 
-  Serial.print("RPM: "); Serial.print(RPM_w); Serial.print(" Speed [m/min]: "); Serial.println(speed);
+    attachInterrupt(digitalPinToInterrupt(SPEED_SENSOR_PIN), count, FALLING);
 
-  data[0] = (RPM_w >> 0) & 0xFF;
-  data[1] = (RPM_w >> 8) & 0xFF;
+    data[0] = (RPM_w >> 0) & 0xFF;
+    data[1] = (RPM_w >> 8) & 0xFF;
 
-  int status = CAN.sendMsgBuf(can_id, 0, can_dlc, data);
-  if (status == CAN_OK) {
-    Serial.println("Success");
-   } else
-    Serial.println("Error");
-   delay(10);
+    status = CAN.sendMsgBuf(can_id, 0, can_dlc, data);
+    if (status == CAN_OK) {
+      Serial.println("Success");
+    } else
+      Serial.println("Error");
+  }
 }
 
-void purseCounter() {
+void pulseCounter() {
   elapsedTime = micros() - prevTime;
   prevTime = micros();
 
@@ -79,7 +78,7 @@ void purseCounter() {
     purseCnt = 1;
     elapsedTimeSum = elapsedTime;
 
-    int tmpReadCnt = map(elapsedTime, 40000, 5000, 1, 10);
+    tmpReadCnt = map(elapsedTime, 40000, 5000, 1, 10);
     readingCnt = constrain(tmpReadCnt, 1, 10);
   } else {
     purseCnt++;
