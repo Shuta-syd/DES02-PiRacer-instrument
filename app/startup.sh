31a7bb0fc094c994f26e7482e79cc7d77814999f
Shuta-syd <shuta.ogura0921@gmail.com>
2023-08-25 19:31:00 +0200

    [update] startup.sh updated

--- app/startup.sh
+++ app/startup.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-DIR = /home/seame01/workspace/DES02-PiRacer-instrument
-
-source $DIR/venv/bin/activate
-python $DIR/app/piracer_py/main.py
+DIR=/home/seame01/workspace/DES02-PiRacer-instrument
+ARDUINO=/home/seame01/bin/arduino-cli
 
 # startup Arduino
-./$DIR/can_modules/setup_can.sh
-ino compile -b arduino:avr:nano:cpu=atmega328old rpm-calculator
-ino upload -b arduino:avr:nano:cpu=atmega328old rpm-calculator -p /dev/ttyUSB0
+$DIR/can-modules/setup_can.sh&
+#$ARDUINO compile -b arduino:avr:nano:cpu=atmega328old rpm-calculator
+#$ARDUINO upload -b arduino:avr:nano:cpu=atmega328old rpm-calculator -p /dev/ttyUSB0
 
 # startup d-bus service & monitor
-./$DIR/app/piracer_py/startup_main.sh&
-./$DIR/app/piracer_py/startup_monitor.sh&
+$DIR/app/piracer_py/startup_main.sh&
+$DIR/app/piracer_py/startup_monitor.sh&
 
 # startup to build application
-./$DIR/app/dashboard/build.sh&
-./$DIR/app/dashboard/clean.sh&
+$DIR/app/dashboard/build.sh&
