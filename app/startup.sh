88401c9da402d437c30da7e48a36fd47c103ae30
Shuta-syd <shuta.ogura0921@gmail.com>
2023-08-31 16:46:52 +0200

    [update] startup.sh updated

--- app/startup.sh
+++ app/startup.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 DIR=$(pwd)
+PARENT_DIR="$(cd .. && pwd)"
+cd $DIR
 
 # startup Arduino
-chmod 755 $DIR/can-modules/setup_can.sh
+chmod 755 $PARENT_DIR/can-modules/setup_can.sh
 chmod 755 $DIR/piracer_py/startup_py.sh
 chmod 755 $DIR/piracer_py/startup_monitor.sh
 chmod 755 $DIR/dashboard/build.sh
 
-$DIR/can-modules/setup_can.sh&
-#$ARDUINO compile -b arduino:avr:nano:cpu=atmega328old rpm-calculator
-#$ARDUINO upload -b arduino:avr:nano:cpu=atmega328old rpm-calculator -p /dev/ttyUSB0
 
-# startup d-bus service & monitor
+$PARENT_DIR/can-modules/setup_can.sh&
+# #$ARDUINO compile -b arduino:avr:nano:cpu=atmega328old rpm-calculator
+# #$ARDUINO upload -b arduino:avr:nano:cpu=atmega328old rpm-calculator -p /dev/ttyUSB0
+
+# # startup d-bus service & monitor
 $DIR/app/piracer_py/startup_py.sh&
 $DIR/app/piracer_py/startup_monitor.sh&
 
-# startup to build application
+# # startup to build application
 $DIR/app/dashboard/build.sh&
